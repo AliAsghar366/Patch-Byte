d3f58fffeca39b249ff2d071163b6633f779ebb0
Your Name <you@example.com>
2026-08-07 23:14:29 +0500

    Fix malformed image URLs in all HTML files

--- fix-urls.ps1
+++ fix-urls.ps1
@@ -1,23 +1,34 @@
-$base = "C:\Users\MOS\Desktop\New folder (2)\frontend\patchkraze.com"
+$base = "d:\zeesha\Patch-Byte-main\Patch-Byte-main\frontend\patchkraze.com"
 
-$dirs = @("$base\pages","$base\policies","$base\collections","$base\products","$base\blogs")
+$dirs = @("$base\pages","$base\policies","$base\collections","$base\products","$base\blogs","$base\cart","$base\checkout")
 $files = @()
 foreach ($dir in $dirs) {
     if (Test-Path $dir) {
         Get-ChildItem $dir -Recurse -Filter "*.html" -ErrorAction SilentlyContinue |
-            Where-Object { $_.Name -notmatch '@variant|@page|oembed|atom' } |
             ForEach-Object { $files += $_ }
     }
 }
 
+# Also add index.html
+$files += Get-Item "$base\index.html" -ErrorAction SilentlyContinue
+
 Write-Host "Fixing $($files.Count) files..."
 foreach ($f in $files) {
     $html = [System.IO.File]::ReadAllText($f.FullName, [System.Text.Encoding]::UTF8)
-    $html = $html -replace '//patchkraze\.com/cdn/', '/cdn/'
-    $html = $html -replace 'https://patchkraze\.com/cdn/', '/cdn/'
-    $html = [regex]::Replace($html, '(/cdn/[^"'' \s>]+)\?[^"'' \s>]*', '$1')
-    $html = $html -replace 'https://patchkraze\.com/(products|collections|pages|blogs|policies|cart|account)/', '/$1/'
-    $html = $html -replace 'https://patchkraze\.com/', '/'
+    
+    # Fix malformed http:/cdn/ URLs
+    $html = $html -replace 'http:/cdn/shop/files/', 'http://cdn.shopify.com/s/files/1/0661/2965/7940/files/'
+    $html = $html -replace 'https:/cdn/shop/files/', 'https://cdn.shopify.com/s/files/1/0661/2965/7940/files/'
+    
+    # Fix malformed https:/cdn/shopifycloud/ URLs
+    $html = $html -replace 'https:/cdn/shopifycloud/', 'https://cdn.shopifycloud/'
+    
+    # Fix malformed http:/cdn/ URLs (general case)
+    $html = $html -replace 'http:/cdn/', 'http://cdn.shopify.com/'
+    
+    # Fix malformed https:/cdn/ URLs (general case)
+    $html = $html -replace 'https:/cdn/', 'https://cdn.shopify.com/'
+    
     [System.IO.File]::WriteAllText($f.FullName, $html, [System.Text.Encoding]::UTF8)
 }
 Write-Host "All done."
